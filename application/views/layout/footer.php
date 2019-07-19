c51001528f50b5d9f8f6a54da233bc34c11e6288
Luis García <luis_grdz@hotmail.com>
2019-07-18 23:23:42 -0500

    subida de la plantilla AdminLTE-2.4.15 la cual se puede utilizar o eliminar según se requiera y utilizar la versión anterior.

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -90,11 +90,11 @@
 <!-- REQUIRED JS SCRIPTS -->
 
 <!-- jQuery 3 -->
-<script src="<?php echo base_url('assets/template/bower_components/jquery/dist/jquery.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/template2/bower_components/jquery/dist/jquery.min.js'); ?>"></script>
 <!-- Bootstrap 3.3.7 -->
-<script src="<?php echo base_url('assets/template/bower_components/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/template2/bower_components/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
 <!-- AdminLTE App -->
-<script src="<?php echo base_url('assets/template/dist/js/adminlte.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/template2/dist/js/adminlte.min.js'); ?>"></script>
 
 <!-- Optionally, you can add Slimscroll and FastClick plugins.
      Both of these plugins are recommended to enhance the
